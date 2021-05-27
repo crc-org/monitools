6ea8bf665f59aa75b3dcd54124cd316ac01df1f3
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-05-27 10:51:46 +0200

    Fix container entrypoint with monictl execution and data retrieve (also added support for ssh with passwd)

--- images/build/entrypoint.sh
+++ images/build/entrypoint.sh
@@ -3,32 +3,54 @@
 # Vars
 MONICTL_PATH=/usr/local/bin/monictl
 RESULTS_PATH="${RESULTS_PATH:-/output}"
+MONICTL_REPETITIONS="${MONICTL_REPETITIONS:-5}"
+MONICTL_INTERVAL="${MONICTL_INTERVAL:-1}"
 
-# Check required Envs
-if [[ -z "${TARGET_HOST}" ]]; then
-    printf "Please set up TARGET_HOST Env\n"
-    exit 1
+if [ "${DEBUG:-}" = "true" ]; then
+    set -xuo 
 fi
 
-if [[ -z "${TARGET_HOST_USERNAME}" ]]; then
-    printf "Please set up TARGET_HOST_USERNAME Env\n"
-    exit 1
-fi
+# Validate conf
+validate=true
+[[ -z "${TARGET_HOST}" ]] \
+    && echo "TARGET_HOST requried" \
+    && validate=false
 
-if [[ -z "${TARGET_HOST_KEY_PATH}" ]]; then
-    printf "Please set up TARGET_HOST_KEY_PATH Env\n"
-    exit 1
-fi
+[[ -z "${TARGET_HOST_USERNAME}" ]] \
+    && echo "TARGET_HOST_USERNAME requried" \
+    && validate=false
+
+[[ -z "${TARGET_HOST_KEY_PATH}" && -z "${TARGET_HOST_PASSWORD}" ]] \
+    && echo "TARGET_HOST_KEY_PATH or TARGET_HOST_PASSWORD required" \
+    && validate=false
+[[ $validate == false ]] && exit 1
+
+# Set SCP / SSH command
+NO_STRICT='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+[[ ! -z "${TARGET_HOST_KEY_PATH}" ]] \
+    && SCP="scp ${NO_STRICT} -i ${TARGET_HOST_KEY_PATH}" \
+        || SCP="sshpass -p ${TARGET_HOST_PASSWORD} scp ${NO_STRICT}" \
+    && SSH="ssh ${NO_STRICT} -i ${TARGET_HOST_KEY_PATH}" \
+        || SSH="sshpass -p ${TARGET_HOST_PASSWORD} ssh ${NO_STRICT}"
+
+# Create execution folder 
+EXECUTION_FOLDER="/home/${TARGET_HOST_USERNAME}/monitools/${RANDOM}"
+DATA_FOLDER="${EXECUTION_FOLDER}/data"
+$SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "mkdir -p ${EXECUTION_FOLDER} && mkdir -p ${DATA_FOLDER}" 
 
 # Copy monictl to target host
-scp -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" \
-    -i "${TARGET_HOST_KEY_PATH}" \
-    "${MONICTL_PATH}" "${TARGET_HOST_USERNAME}@${TARGET_HOST}:/home/${TARGET_HOST_USERNAME}"
+$SCP "${MONICTL_PATH}" "${TARGET_HOST_USERNAME}@${TARGET_HOST}:${EXECUTION_FOLDER}"
 
 # Run (one shot) monictl
-#ssh -i "${TARGET_HOST_KEY_PATH}" "${TARGET_HOST_USERNAME}@${TARGET_HOST}" /home/${TARGET_HOST_USERNAME}/monictl
+MONITOOL_EXEC="${EXECUTION_FOLDER}/monictl -d ${DATA_FOLDER} -n ${MONICTL_REPETITIONS} -s ${MONICTL_INTERVAL}"
+$SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "${MONITOOL_EXEC}"
+
+# Wait for execution
+sleep $(($MONICTL_REPETITIONS*$MONICTL_INTERVAL))
 
 # Get results
-#mkdir -p "${RESULTS_PATH}"
-#scp -r -i "${TARGET_HOST_KEY_PATH}" \
-#    "${TARGET_HOST_USERNAME}@${TARGET_HOST}:/home/${TARGET_HOST_USERNAME}/monictl-results" "${RESULTS_PATH}"
+mkdir -p "${RESULTS_PATH}"
+$SCP -r "${TARGET_HOST_USERNAME}@${TARGET_HOST}:${DATA_FOLDER}" "${RESULTS_PATH}"
+
+# Remove remote execution fodler
+$SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" rm -rf "${EXECUTION_FOLDER}"
