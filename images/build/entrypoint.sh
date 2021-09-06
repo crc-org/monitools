ca93929237eff9b5519ff807cc28e88b21c9107f
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-09-06 09:09:04 +0200

    added support for looping monitools execution on target for a period with an interval

--- images/build/entrypoint.sh
+++ images/build/entrypoint.sh
@@ -5,6 +5,7 @@ MONICTL_PATH=/usr/local/bin/monictl
 RESULTS_PATH="${RESULTS_PATH:-/output}"
 MONICTL_REPETITIONS="${MONICTL_REPETITIONS:-5}"
 MONICTL_INTERVAL="${MONICTL_INTERVAL:-1}"
+MONITOOL_INTERVAL="${MONITOOL_INTERVAL:-30m}"
 
 if [ "${DEBUG:-}" = "true" ]; then
     set -xuo 
@@ -23,6 +24,7 @@ validate=true
 [[ -z "${TARGET_HOST_KEY_PATH}" && -z "${TARGET_HOST_PASSWORD}" ]] \
     && echo "TARGET_HOST_KEY_PATH or TARGET_HOST_PASSWORD required" \
     && validate=false
+
 [[ $validate == false ]] && exit 1
 
 # Set SCP / SSH command
@@ -41,9 +43,23 @@ $SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "mkdir -p ${EXECUTION_FOLDER} && m
 # Copy monictl to target host
 $SCP "${MONICTL_PATH}" "${TARGET_HOST_USERNAME}@${TARGET_HOST}:${EXECUTION_FOLDER}"
 
-# Run (one shot) monictl
-MONITOOL_EXEC="${EXECUTION_FOLDER}/monictl -d ${DATA_FOLDER} -n ${MONICTL_REPETITIONS} -s ${MONICTL_INTERVAL}"
-$SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "${MONITOOL_EXEC}"
+if [[ -z "${MONITOOL_PERIOD}" ]]; then
+    # Run (one shot) monictl
+    MONITOOL_EXEC="${EXECUTION_FOLDER}/monictl -d ${DATA_FOLDER} -n ${MONICTL_REPETITIONS} -s ${MONICTL_INTERVAL}"
+    $SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "${MONITOOL_EXEC}"
+else 
+    START=$(date +%s)
+    TOTAL_TIME=$((60 * 60 * ${MONITOOL_PERIOD}))
+    UPTIME=$(($(date +%s) - $START))
+    ITERATION=0
+    while [[ $UPTIME -lt $TOTAL_TIME ]]; do
+        MONITOOL_EXEC="${EXECUTION_FOLDER}/monictl -d "${DATA_FOLDER}/${ITERATION}" -n ${MONICTL_REPETITIONS} -s ${MONICTL_INTERVAL}"
+        $SSH "${TARGET_HOST_USERNAME}@${TARGET_HOST}" "${MONITOOL_EXEC}"
+        UPTIME=$(($(date +%s) - $START))
+        ((ITERATION++))
+        sleep ${MONITOOL_INTERVAL}
+    done
+fi
 
 # Get results
 mkdir -p "${RESULTS_PATH}"
